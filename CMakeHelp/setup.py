ff6d30e25563f9b2f39290c94323ee5162598ff1
tomek <tomek@ncomputing.com>
2010-01-01 01:49:11 -0500

    Created setuptools package

---
+++ CMakeHelp/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='CMakeHelp',
+      version=version,
+      description="CMake Interactive Help System",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='cmake interactive help system command line',
+      author='Tomasz Bogdal',
+      author_email='vladsmailtotomas@gmail.com',
+      url='http://www.qtzlabs.com/',
+      license='LGPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
